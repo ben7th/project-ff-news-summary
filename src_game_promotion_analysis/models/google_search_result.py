dfa8472f4d31c4051a3f088e347f7d315a4122cf
ben7th <ben7th@sina.com>
2023-07-14 13:51:57 +0800

    update t01 s01 search_google

--- src_game_promotion_analysis/models/google_search_result.py
+++ src_game_promotion_analysis/models/google_search_result.py
@@ -1,9 +1,11 @@
-from mongoengine import Document, StringField, DateTimeField
+from mongoengine import Document, StringField, DateTimeField, IntField
 from datetime import datetime
 
 # MongoDB 模型
 class GoogleSearchResult(Document):
-    search_url = StringField(required=True)
-    search_result_html = StringField(required=True)
     created_at = DateTimeField(default=datetime.utcnow)
     updated_at = DateTimeField(default=datetime.utcnow)
+
+    search_url = StringField(required=True)
+    search_result_html = StringField(required=True)
+    result_items_count = IntField(required=True)
--- src_game_promotion_analysis/scripts/t01_search/s01_search_google.py
+++ src_game_promotion_analysis/scripts/t01_search/s01_search_google.py
@@ -15,32 +15,46 @@ async def get_page_html(url):
     page = await browser.newPage()
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')
-    await page.goto(url, timeout=60000)
-    await page.waitForSelector('#search')
+    await page.goto(url, timeout=60000, waitUntil='networkidle2')
 
     html_content = await page.evaluate('''() => {
         return document.querySelector("#search").outerHTML;
     }''')
 
+    result_items_count = await page.evaluate('''() => {
+        return document.querySelectorAll('#search h3').length
+    }''')
+
+    print(f'获得 {result_items_count} 条结果')
+
     await browser.close()
 
-    return html_content
+    return html_content, result_items_count
 
 # 保存搜索结果
-def save_search_result(url, html_content):
+def save_search_result(url, html_content, result_items_count):
     search_result = GoogleSearchResult(
         search_url=url,
-        search_result_html=html_content
+        search_result_html=html_content,
+        result_items_count=result_items_count,
     )
     search_result.save()
 
-# 主程序入口
+# 调用搜索
 async def main(url):
     connect_to_db()
-    html_content = await get_page_html(url)
-    save_search_result(url, html_content)
+    html_content, result_items_count = await get_page_html(url)
+    save_search_result(url, html_content, result_items_count)
+
+# 组织搜索 url
+def search(keyword):
+    print(f'开始搜索 {keyword}')
+    url = f'https://www.google.com/search?q={keyword}&num=100'
+    asyncio.run(main(url))
+
+# if __name__ == '__main__':
+#     search('最终幻想16')
 
 if __name__ == '__main__':
-    # 调用主程序
-    url = 'https://www.google.com/search?q=final+fantasy+16&num=100'
-    asyncio.get_event_loop().run_until_complete(main(url))
+    search_keyword = input("请输入搜索关键词: ")
+    search(search_keyword)
